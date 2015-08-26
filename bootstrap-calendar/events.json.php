abbda8e6ef378a67fca77ee678feac58ad1122ee
zacharyfmarion <zachary.marion@duke.edu>
2015-08-26 12:20:19 -0400

    Added events folder

--- bootstrap-calendar/events.json.php
+++ bootstrap-calendar/events.json.php
@@ -3,8 +3,8 @@
 	"result": [
 		{
 			"id": "293",
-			"title": "This is warning class event with very long title to check how it fits to evet in day view",
-			"url": "http://www.example.com/",
+			"title": "This is warning class event with very long title to check how it fits to event in day view",
+			"url": "http://www.lowellringel.com/events/test.html",
 			"class": "event-info",
 			"start": "1433937600000",
 			"end": "1433941200000"
