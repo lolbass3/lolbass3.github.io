b8d44f28076ac450bef501d9dbe7bf75e8986134
Lowell Ringel <lowellringel@Administrators-MacBook-Pro.local>
2015-08-27 10:05:02 -0400

    bleh

--- bootstrap-calendar/events.json.php
+++ bootstrap-calendar/events.json.php
@@ -8,14 +8,6 @@
 			"class": "event-info",
 			"start": "1440630000000",
 			"end": "1440644400000"
-		},
-
-		{
-			"id": "3",
-			"title": "duo with arianna neikrug",
-			"url": "http://www.lowellringel.com/events/event_3.html",
-			"class": "event-info",
-			"start": "1440808200000",
-			"end": "1440815400000"
+		}
 	]
 }
