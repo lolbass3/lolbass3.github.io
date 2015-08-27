6c4ac49ee128d24f3d1f894db242f68532aab632
Lowell Ringel <lowellringel@Administrators-MacBook-Pro.local>
2015-08-27 17:27:50 -0400

    adding more events

--- bootstrap-calendar/events.json.php
+++ bootstrap-calendar/events.json.php
@@ -16,6 +16,22 @@
 			"class": "event-info",
 			"start": "1440808200000",
 			"end": "1440815400000"
+		},
+		{
+			"id": "4",
+			"title": "private party with jonah udall quartet",
+			"url": "http://www.lowellringel.com/events/event_4.html",
+			"class": "event-info",
+			"start": "1440889200000",
+			"end": "1440903600000"
+		},
+		{
+			"id": "5",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_5.html",
+			"class": "event-info",
+			"start": "1440950400000",
+			"end": "1440961200000"
 		}
 	]
 }
