dc7bbf2a4e5cfb548e9c649478c2325880fa76e5
Lowell Ringel <lowellringel@Administrators-MacBook-Pro.local>
2015-08-30 19:21:13 -0400

    adding more events

--- bootstrap-calendar/events.json.php
+++ bootstrap-calendar/events.json.php
@@ -32,6 +32,62 @@
 			"class": "event-info",
 			"start": "1440950400000",
 			"end": "1440961200000"
+		},
+		{
+			"id": "6",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_6.html",
+			"class": "event-info",
+			"start": "1441555200000",
+			"end": "1441566000000"
+		},
+		{
+			"id": "7",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_7.html",
+			"class": "event-info",
+			"start": "1442160000000",
+			"end": "1442170800000"
+		},
+		{
+			"id": "8",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_8.html",
+			"class": "event-info",
+			"start": "1443974400000",
+			"end": "1443985200000"
+		},
+		{
+			"id": "9",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_9.html",
+			"class": "event-info",
+			"start": "1444579200000",
+			"end": "1444590000000"
+		},
+		{
+			"id": "10",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_10.html",
+			"class": "event-info",
+			"start": "1445184000000",
+			"end": "1445194800000"
+		},
+		{
+			"id": "11",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_11.html",
+			"class": "event-info",
+			"start": "1445788800000",
+			"end": "1445799600000"
+		},
+		{
+			"id": "12",
+			"title": "duo with chantil dukart",
+			"url": "http://www.lowellringel.com/events/event_12.html",
+			"class": "event-info",
+			"start": "1446397200000",
+			"end": "1446408000000"						
 		}
 	]
 }
